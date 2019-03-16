5004fc497acb5d0b2d3c80cdc1f942736723d605
Corey Fanger <coreyf95@gmail.com>
2019-03-15 21:39:14 -0500

    Combined example main classes into a single connection handler

--- src/com/dakafak/simpleclients/examples/ServerExample.java
+++ src/com/dakafak/simpleclients/examples/ServerExample.java
@@ -1,18 +1,21 @@
-package com.dakafak.simpleclients.examples.terminal;
+package com.dakafak.simpleclients.examples;
 
+import com.dakafak.simpleclients.examples.ping.PingTask;
+import com.dakafak.simpleclients.examples.terminal.ActionRecord;
+import com.dakafak.simpleclients.examples.terminal.ActionTask;
 import com.dakafak.simpleclients.server.ConnectionHandler;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static com.dakafak.simpleclients.examples.MyPayloadTypes.ACTION;
 
-public class TerminalServerExample {
+public class ServerExample {
 
 	public static void main(String[] args) {
 		ConcurrentLinkedQueue<ActionRecord> actionRecords = new ConcurrentLinkedQueue<>();
 		ConnectionHandler connectionHandler = new ConnectionHandler(1776);
-		ActionTask actionTask = new ActionTask(actionRecords);
-		connectionHandler.addTask(ACTION, actionTask);
+		connectionHandler.addTask(ACTION, new ActionTask(actionRecords));
+		connectionHandler.addTask(MyPayloadTypes.PING, new PingTask());
 		connectionHandler.startListeningForConnections();
 	}
 
